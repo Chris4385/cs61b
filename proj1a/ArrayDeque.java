0fee6146e160c2ff3703c005063be72aac5205a5
Chris4385 <ganboontoh@gmail.com>
2021-05-09 18:39:20 +0800

    first submission of project 1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,122 @@
+public class ArrayDeque<Item> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private int initialArrSize = 4;
+
+    private Item[] items;
+
+    public ArrayDeque() {
+        items = (Item[]) new Object[initialArrSize];
+        size = 0;
+        int mid = initialArrSize / 2;
+        nextFirst = mid - 1;
+        nextLast = mid;
+    }
+
+    public ArrayDeque(ArrayDeque<Item> other) {
+        nextFirst = other.nextFirst;
+        nextLast = other.nextLast;
+        size = other.initialArrSize;
+        items = (Item[]) new Object[size];
+        System.arraycopy(other.items, 0, items, 0, size);
+
+    }
+
+    //if the index reaches the max limit of the array size, reset the index into 0
+    private int resetToFront(int x) {
+        if (x > initialArrSize - 1) {
+            return 0;
+        } else {
+            return x;
+        }
+    }
+
+    //if the index reaches the lower limit of 0, reset the index into array size
+
+    private int resetToEnd(int x) {
+        if (x < 0) {
+            return initialArrSize - 1;
+            /** because intialArrSize is 8, but index starts from 0 to 7 so the max index is 7 */
+        } else {
+            return x;
+        }
+    }
+
+    private void resizeCapacity(int capacity) {
+        Item[] newItems = (Item[]) new Object[capacity];
+        int originalSize = items.length;
+        int remainingSpace = capacity - originalSize;
+        initialArrSize = capacity;
+        nextFirst = (remainingSpace / 2) - 1;
+        nextLast = (remainingSpace / 2) + originalSize;
+        System.arraycopy(items, 0, newItems, nextFirst + 1, originalSize);
+        items = newItems;
+    }
+
+    private int maxArraySize(int x) {
+        if (x == initialArrSize - 1) {
+            resizeCapacity(initialArrSize * 2);
+        }
+        return x + 1;
+    }
+
+
+    public void addLast(Item x) {
+        items[nextLast] = x;
+        nextLast = resetToFront(nextLast + 1);
+        size = maxArraySize(size);
+    }
+
+    public void addFirst(Item x) {
+        items[nextFirst] = x;
+        nextFirst = resetToEnd(nextFirst - 1);
+        size = maxArraySize(size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printDeque() {
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public Item get(int index) {
+        if (isEmpty()) {
+            return null;
+        }
+        return items[index - 1];
+    }
+
+//    public static void main(String[] args) {
+//        ArrayDeque<Integer> L = new ArrayDeque<>();
+//        L.addFirst(1);
+//        L.addFirst(2);
+//        L.addFirst(3);
+//        L.addFirst(4);
+//        L.addFirst(5);
+//        L.addFirst(6);
+//
+//        L.addLast(100);
+//        L.addLast(200);
+//        L.addLast(300);
+//        L.addLast(400);
+//        L.addLast(500);
+//
+//        L.addFirst(7);
+//        L.addFirst(8);
+//        L.addFirst(9);
+//        L.addFirst(10);
+//        L.addFirst(11);
+//
+//        L.addLast(600);
+
+//    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,139 @@
+public class LinkedListDeque<Item> {
+
+    private static class Node<Item> {
+        private Node<Item> head;
+        private Node<Item> tail;
+        private Item data;
+
+        public Node(Item input, Node<Item> prev, Node<Item> next) {
+            data = input;
+            head = prev;
+            tail = next;
+        }
+
+    }
+
+    private Node<Item> sentinelA;
+    private Node<Item> sentinelB;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinelA = new Node<>(null, null, null);
+        sentinelB = new Node<>(null, null, null);
+        sentinelA.tail = sentinelB;
+        sentinelB.head = sentinelA;
+        size = 0;
+    }
+
+    public LinkedListDeque(Item item) {
+        sentinelA = new Node<>(null, null, null);
+        sentinelB = new Node<>(null, null, null);
+
+        Node<Item> newData = new Node<>(item, sentinelA, sentinelB);
+        sentinelA.tail = newData;
+        sentinelB.head = newData;
+        size = 1;
+
+    }
+
+    public LinkedListDeque(LinkedListDeque<Item> other) {
+        this();
+        int size = other.size();
+        Node<Item> p = other.sentinelA.tail;
+
+        for (int i = 0; i < size; i++) {
+
+            this.addLast(p.data);
+            p = p.tail;
+        }
+    }
+
+    public void addFirst(Item item) {
+        Node<Item> data = new Node<>(item, sentinelA, sentinelA.tail);
+        sentinelA.tail = data;
+        data.tail.head = data;
+        size++;
+    }
+
+    public void addLast(Item item) {
+        Node<Item> data = new Node<>(item, sentinelB.head, sentinelB);
+        sentinelB.head = data;
+        data.head.tail = data;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Node<Item> firstNodeAfterDelete = sentinelA.tail.tail;
+        sentinelA.tail = firstNodeAfterDelete;
+        firstNodeAfterDelete.head = sentinelA;
+        size--;
+        return firstNodeAfterDelete.data;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node<Item> lastNodeAfterDelete = sentinelB.head.head;
+        sentinelB.head = lastNodeAfterDelete;
+        lastNodeAfterDelete.tail = sentinelB;
+        size--;
+        return lastNodeAfterDelete.data;
+    }
+
+    public Item get(int index) {
+        if (isEmpty()) {
+            return null;
+        }
+        Node<Item> p = sentinelA;
+        for (int i = 0; i <= index; i++) {
+            if (i != index) {
+                p = p.tail;
+            }
+        }
+        return p.data;
+    }
+
+    private static <Item> Item getNodeRecursive(Node<Item> p, int index) {
+        if (index == 1) {
+            return p.data;
+        } else {
+            return getNodeRecursive(p.tail, index - 1);
+        }
+
+    }
+
+    public Item getRecursive(int index) {
+        if (isEmpty()) {
+            return null;
+        }
+        return getNodeRecursive(sentinelA.tail, index);
+
+
+    }
+
+
+//    public static void main(String[] args) {
+//        LinkedListDeque<Integer> LL = new LinkedListDeque<>(100);
+//        LL.addLast(200);
+//        LL.addLast(300);
+//        LL.addFirst(150);
+//        LL.addFirst(1000);
+//
+//        int a = LL.getRecursive(1);
+//        System.out.println("First item is: " + a + " and its total size is: " + LL.size());
+//    }
+
+}
